d4d2316c9e8a75ce4607b5c3105250ad9ce30c3c
wangkaiwd <1162486176@qq.com>
2018-04-16 20:19:30 +0800

    配置webpack并引入antd design

---
+++ work/src/tabs.js
@@ -0,0 +1,24 @@
+import React,{Component} from 'react';
+import {render} from 'react-dom';
+import { Tabs } from 'antd';
+const TabPane = Tabs.TabPane;
+
+export default class MyTabs extends Component {
+    constructor(){
+        super();
+    }
+    render() {
+        return (
+            <div>
+                <Tabs defaultActiveKey="1">
+                    <TabPane tab="Tab 1" key="1">Content of Tab Pane 1</TabPane>
+                    <TabPane tab="Tab 2" key="2">Content of Tab Pane 2</TabPane>
+                    <TabPane tab="Tab 3" key="3">Content of Tab Pane 3</TabPane>
+                </Tabs>
+            </div>   
+        );
+    }
+}
+
+render(<Tabs />, document.getElementById('box'));
+
