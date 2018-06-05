a6dc5c45a83838d70ab45495435fd71e01c94089
wangkaiwd <1162486176@qq.com>
2018-06-05 18:22:01 +0800

    路由的query和state传参方式和权限控制

--- animation/src/app.js
+++ animation/src/app.js
@@ -8,30 +8,42 @@ class App extends Component {
     this.state = {
 
     }
+    window.auth = 1;
   }
   render() {
     const { action } = this.props.location;
-    console.log(action);
+    const isAnimation = this.props.location.pathname.lastIndexOf('/') === 0;
+    const data = { id: 3, name: 'sam', age: 36 }
+    const path = {
+      pathname: '/home',
+      state: data
+    }
     return (
       <div className="app">
         <ul className="nav">
-          <li><Link to="/home">首页</Link></li>
+          <li><Link to={path}>首页</Link></li>
           <li><Link to="/product">商品</Link></li>
           <button onClick={() => hashHistory.go(-1)}>返回</button>
         </ul>
-        <ReactCSSTransitionGroup
-          component="div"
-          className="react-container"
-          transitionName={action === 'PUSH' ? 'slide-in' : 'slide-out'}
-          transitionEnterTimeout={300}
-          transitionLeaveTimeout={300}
-        >
-          {/* 需要一个唯一的key */}
-          <div style={{ height: '100%' }} key={this.props.location.pathname} className={this.props.location.pathname}>
-            {this.props.children}
-          </div>
-        </ReactCSSTransitionGroup>
-
+        {
+          isAnimation ?
+            <ReactCSSTransitionGroup
+              component="div"
+              className="react-container"
+              transitionName={action === 'PUSH' ? 'slide-in' : 'slide-out'}
+              transitionEnterTimeout={300}
+              transitionLeaveTimeout={300}
+            >
+              {/* 需要一个唯一的key */}
+              <div style={{ height: '100%' }} key={this.props.location.pathname} className={this.props.location.pathname}>
+                {this.props.children}
+              </div>
+            </ReactCSSTransitionGroup>
+            :
+            <div style={{ height: '100%' }}>
+              {this.props.children}
+            </div>
+        }
       </div>
     );
   }
