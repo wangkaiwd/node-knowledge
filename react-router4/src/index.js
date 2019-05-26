7ba155777df700032423b1047a85fe47a7f7c0fb
wangkaiwd <1162486176@qq.com>
2019-05-26 23:00:48 +0800

    feat: 重置仓库内容

--- react-router4/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom'
-import { HashRouter as Router } from 'react-router-dom'
-import App from './app'
-
-render(<Router><App /></Router>, document.getElementById('box'));
--- webpack4.0/postcss.config.js
+++
@@ -1,6 +0,0 @@
-// module.exports = {
-//   plugins: [
-//     // 引用该插件即可为
-//     require('autoprefixer'),
-//   ]
-// }
