90667d54002a513f1a53e965d935e3b376c76018
wangkaiwd <1162486176@qq.com>
2019-10-18 22:54:59 +0800

    feat: log some common use api

--- 03 static-server/statciServer.ts
+++ 03 static-server/statciServer.ts
@@ -1,11 +1,15 @@
 import http from 'http';
 
+// node的ts接口声明写的并不好
 const PORT = 8888;
 const server = http.createServer();
 // http.Server类
 // request：每次请求都会触发
 server.on('request', (request, response) => {
+  const { url, headers, method } = request;
+  console.log('request some property', url, headers, method);
   response.end('hi');
+  console.log('url', request.url);
   console.log('有人请求了');
 });
 // server.listen: 启动HTTP服务器监听连接
