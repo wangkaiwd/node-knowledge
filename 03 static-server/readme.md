f4f4e87e40bb0b07796c8507d0af821543b21713
wangkaiwd <1162486176@qq.com>
2019-10-20 19:48:29 +0800

    feat: how to read node.js document

--- 03 static-server/readme.md
+++ 03 static-server/readme.md
@@ -7,3 +7,14 @@
 * `ts-node-dev`
 
 ### `curl`常用命令
+
+
+### `node.js`文档阅读
+1. 通过`http.createServer`来创建服务
+2. 步骤1执行`http.createServer`后返回一个`http.Server`实例
+3. 阅读`http.Server`文档，里面介绍了`request`事件，并且接收`request`和`response`俩个参数 
+
+处理`post`请求：  
+1. `request`事件中的`request`参数是`http.IncomingMessage`类，其对应的方法大概浏览一遍
+2. 文档中显示`http.IncomingMessage`类又继承自`stream.Readable`
+3. 在`stream.Readable`类中发现`data`和`end`事件
--- 03 static-server/statciServer.ts
+++ 03 static-server/statciServer.ts
@@ -2,9 +2,12 @@ import http from 'http';
 
 // node的ts接口声明写的并不好
 const PORT = 8888;
+// 1. 通过`http.createServer`来创建服务
 const server = http.createServer();
+// 2. 步骤1执行`http.createServer`后返回一个`http.Server`实例
 // http.Server类
 // request：每次请求都会触发
+// 3. 阅读`http.Server`文档，里面介绍了`request`事件，并且接收`request`和`response`俩个参数
 server.on('request', (request, response) => {
   let body = '';
   // 继承自stream.Readable,在这个类上有对应的data和end事件
@@ -20,3 +23,4 @@ server.on('request', (request, response) => {
 server.listen(PORT, () => {
   console.log(`server is listening on ${PORT} port`);
 });
+
