5ca07f29d0ffea39917cec5ad5419e91c10d17da
wangkaiwd <1162486176@qq.com>
2019-10-20 23:45:21 +0800

    feat: handle distinguish request url about file resource

--- 03 static-server/demo02.ts
+++ 03 static-server/demo02.ts
@@ -1,14 +1,17 @@
 import http from 'http';
 import fs from 'fs';
 import path from 'path';
+import * as url from 'url';
 
 const PORT = 8888;
 const server = http.createServer();
 
-const absPath = path.resolve(__dirname, './public/index.html');
+const absPath = (dir: string) => path.resolve(__dirname, `./public${dir}`);
 server.on('request', (request, response) => {
-  const { url } = request;
-  fs.readFile(absPath, (err, data) => {
+  const { url: requestUrl } = request;
+  let { pathname } = url.parse(requestUrl);
+  pathname = pathname === '/' ? '/index.html' : pathname;
+  fs.readFile(absPath(pathname!), (err, data) => {
     if (err) return console.log(err);
     response.end(data);
   });
