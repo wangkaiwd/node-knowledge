18f7a47594098d7aebe0e7f9f9826ad30c37c3d1
wangkaiwd <1162486176@qq.com>
2019-10-22 22:51:07 +0800

    feat: simple file storage

--- 05 fs/demo01.ts
+++ 05 fs/demo01.ts
@@ -1,6 +1,12 @@
 import fs from 'fs';
 import * as path from 'path';
 
+const storePath = path.resolve(__dirname, './test.json');
+const storeObject = {
+  k1: 'v1',
+  k2: 'v2'
+};
+
 const isFileExist = (path: string, callback: (isExist: boolean) => void): void => {
   // tests a user's permissions for the file or directory specified by path
   fs.access(path, fs.constants.F_OK, (err) => {
@@ -8,6 +14,18 @@ const isFileExist = (path: string, callback: (isExist: boolean) => void): void =
   });
 };
 
-isFileExist(path.resolve(__dirname, './test.ts'), (isExist) => {
-  console.log('isExist', isExist);
+isFileExist(path.resolve(storePath), (isExist) => {
+  if (!isExist) {
+    // asynchronously append data to a file, creating the file if ti does not exist. data can be a string or a Buffer
+    fs.appendFile(storePath, JSON.stringify(storeObject), (err) => {
+      if (err) console.log(err);
+    });
+  } else {
+    fs.readFile(storePath, (err, data) => {
+      if (err) console.log(err);
+      if (data) {
+        console.log(data.toString());
+      }
+    });
+  }
 });
