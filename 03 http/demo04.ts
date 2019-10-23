f15e2d7447e43919fa2e26bb0f36726ee55bef19
wangkaiwd <1162486176@qq.com>
2019-10-23 22:55:14 +0800

    feat: proxy always request failed

---
+++ 03 http/demo04.ts
@@ -0,0 +1,41 @@
+import http, { IncomingMessage } from 'http';
+import * as https from 'https';
+
+const PORT = 8888;
+const server = http.createServer();
+
+server.on('request', (request: IncomingMessage, response) => {
+  const { method, headers, url } = request;
+  const requestProxy = http.request({
+    method,
+    headers,
+    path: url,
+    host: 'cnodejs.org'
+  }, (responseProxy: IncomingMessage) => {
+    const { headers } = responseProxy;
+    for (const key in headers) {
+      if (headers.hasOwnProperty(key)) {
+        response.setHeader(key, headers[key]);
+      }
+    }
+    responseProxy.on('data', (chunk) => {
+      response.write(chunk);
+    });
+    responseProxy.on('end', () => {
+      response.end();
+    });
+  });
+  request.on('data', (chunk) => {
+    requestProxy.write(chunk);
+  });
+  request.on('end', () => {
+    requestProxy.end();
+  });
+  requestProxy.on('error', (err) => {
+    console.log('requestProxy error', err);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`server is listening on port ${PORT}`);
+});
