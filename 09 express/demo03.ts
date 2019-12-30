1255d31503482e94f9155101e89febabca6b7e0d
wangkaiwd <1162486176@qq.com>
2019-12-30 23:26:35 +0800

    feat: middleware syntactic sugar

--- 09 express/demo03.ts
+++ 09 express/demo03.ts
@@ -11,6 +11,11 @@ app.use((req, res, next) => {
   next();
 });
 
+// express provides syntactic sugar for corresponding writing, without us have to write various conditional judgments
+app.get('/xxx', (req, res) => {
+  res.send('xxx');
+});
+
 app.listen(PORT, () => {
   console.log(`server is listening on port ${PORT}`);
 });
