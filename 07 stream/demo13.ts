49928dd1a4e23f9f441b118e94f3920228709215
wangkaiwd <1162486176@qq.com>
2020-01-12 00:17:59 +0800

    feat: this direction of setTimeout

--- 07 stream/demo13.ts
+++ 07 stream/demo13.ts
@@ -1,8 +1,15 @@
 import fs from 'fs';
 
-const readableStream = fs.createReadStream('../yarn.lock');
+// todo:
+//  1. 创建可读流文档几句话翻译
+//  2. setTime(obj.fn,1000) vs setTime(() => obj.fn(),1000) ： this指向问题
+const readableStream = fs.createReadStream('../yarn.lock', {
+  highWaterMark: 1024,
+});
+let count = 0;
 readableStream.on('data', (chunk) => {
   console.log(chunk.toString());
+  console.log(++count);
 });
 readableStream.on('end', () => {
   console.log('done');
