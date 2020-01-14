df550afa340d5a39ac381f5eb42661416d29d4ba
wangkaiwd <1162486176@qq.com>
2020-01-14 23:43:04 +0800

    feat: buffer demo

---
+++ 11 buffer/demo01.ts
@@ -0,0 +1,8 @@
+const buffer = Buffer.alloc(20);
+const buffer2 = Buffer.allocUnsafe(20);
+console.log('buffer');
+console.log(buffer);
+
+console.log('buffer2');
+console.log(buffer2);
+
---
+++ 11 buffer/readme.md
@@ -0,0 +1,6 @@
+### `Buffer`
+* `Node.js`中用来处理不知道编码的二进制数据
+* 自己单独管理一片内存
+
+#### `API`
+* Buffer.alloc: 为`Buffer`分配内存 
