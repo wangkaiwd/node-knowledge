d9269f8ecdf443173e81a168ecc4d5eba7396017
wangkaiwd <1162486176@qq.com>
2020-01-17 00:11:25 +0800

    feat: add first http response

---
+++ egg/scaffold/app/controller/home.ts
@@ -0,0 +1,13 @@
+import { Controller } from 'egg';
+
+export default class HomeController extends Controller {
+  public async index () {
+    const { ctx } = this;
+    ctx.body = await ctx.service.test.sayHi('egg');
+  }
+
+  public async test () {
+    const { ctx } = this;
+    ctx.body = 'I am a test text!';
+  }
+}
