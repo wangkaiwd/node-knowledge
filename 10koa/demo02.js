72c257ea648d0adbabddbc817c38c7a98a20c3c3
wangkaiwd <1162486176@qq.com>
2020-08-04 23:20:10 +0800

    feat: little complex demo of koa middleware

---
+++ 10koa/demo02.js
@@ -0,0 +1,23 @@
+const Koa = require('koa');
+const app = new Koa();
+
+const PORT = 3000;
+// 计算接口响应时间
+app.use(async (ctx, next) => {
+  await next();
+  const time = ctx.response.get('X-Response-Time');
+  console.log(`${ctx.url} -- ${time}`);
+});
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const time = Date.now() - start;
+  ctx.set('X-Response-Time', `${time}ms`);
+});
+app.use(async (ctx, next) => {
+  ctx.body = 'hello world';
+});
+app.listen(PORT, () => {
+  console.log(`Koa started in port ${PORT}`);
+});
